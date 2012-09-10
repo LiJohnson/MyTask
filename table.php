60b7047284cc2d0d103d460c3151ddcd0c7325fc
LiJohnson <598420668@163.com>
2012-09-10 01:42:59 +0000

    git-svn-id: https://svn.sinaapp.com/theotherdoor/2/myapp/task@309 8ae2ba1d-993d-4c9a-aded-4246d96bdaa4

--- table.php
+++ table.php
@@ -3,20 +3,20 @@
 class Record{
 	var $table_name = 'record';
 	var $id;
-        var $mid ;
-        var $cid;
+	var $mid ;
+	var $cid;
 }
 
 class Task
 {
 	var $table_name = 'task';
-        var $id ;
-        var $uid ;
-        var $eid ;
-        var $text ;
-        var $pic ;
-        var $time ;
-        var $done ;
+	var $id ;
+	var $uid ;
+	var $eid ;
+	var $text ;
+	var $pic ;
+	var $time ;
+	var $done ;
 }
         
 ?>
