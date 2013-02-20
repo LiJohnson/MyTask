905ef3ed8c14078aa4998bc46a725382966f4250
LiJohnson <598420668@163.com>
2013-02-20 03:13:31 +0000

    git-svn-id: https://svn.sinaapp.com/theotherdoor/2/myapp/task@470 8ae2ba1d-993d-4c9a-aded-4246d96bdaa4

---
+++ config.php
@@ -0,0 +1,30 @@
+<?php
+
+if( !defined("SAE_TMP_PATH") ) //SAE_TMP_PATH 为sae的预定义变量，用来区分本地环境还是sae环境
+{
+	define(	"SRC_PATH" , 'http://lcs.com/sae/gtbcode/1/');
+}
+else
+{
+	define(	"SRC_PATH" , 'http://1.gtbcode.sinaapp.com/');
+}
+
+function getScript($str)
+{
+	$path = SRC_PATH.'js/';
+	$files = preg_split("/\,/" , $str);
+	foreach( $files as $f )
+	{
+		echo "<script src='$path$f' ></script>";
+	}
+}
+function getCss($str)
+{
+	$path = SRC_PATH.'css/';
+	$files = preg_split("/,/" , $str);
+	foreach( $files as $f )
+	{
+		echo "<link rel='stylesheet' href='$path$f' ></link>";
+	}
+}
+
