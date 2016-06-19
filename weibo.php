61b29f774efcd325146e4a3a0bdf40285542982e
LiJohnson <598420668@163.com>
2016-06-19 17:24:00 +0800

    add icon

--- weibo.php
+++ weibo.php
@@ -46,6 +46,9 @@ if( $_GET['login'] ){
 	<title>weibo</title>
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="apple-touch-icon" sizes="114x114" href="icon.png" />
+	<link rel="icon" sizes="114x114" href="icon.png" />
+
 	<link rel="stylesheet" href="http://cdn.bootcss.com/bootstrap/3.3.5/css/bootstrap.min.css">
 	<script src="http://cdn.bootcss.com/jquery/1.11.3/jquery.min.js"></script>
 	<script src="http://cdn.bootcss.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
