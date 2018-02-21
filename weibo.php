3f7ca912f33c585ef5416cc0fbc3f681684a29ec
LiJohnson <598420668@163.com>
2018-02-22 01:57:11 +0800

    兼容https

--- weibo.php
+++ weibo.php
@@ -42,9 +42,9 @@ if( $_GET['login'] ){
 	<link rel="apple-touch-icon" sizes="114x114" href="icon.png" />
 	<link rel="icon" sizes="114x114" href="icon.png" />
 
-	<link rel="stylesheet" href="http://cdn.bootcss.com/bootstrap/3.3.5/css/bootstrap.min.css">
-	<script src="http://cdn.bootcss.com/jquery/1.11.3/jquery.min.js"></script>
-	<script src="http://cdn.bootcss.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="//cdn.bootcss.com/bootstrap/3.3.5/css/bootstrap.min.css">
+	<script src="//cdn.bootcss.com/jquery/1.11.3/jquery.min.js"></script>
+	<script src="//cdn.bootcss.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 </head>
 <body>
 	<div class="container">
